8040538836920901d09d1abae6840bc88c01f87a
betuldemirkan <95471901+betuldemirkan@users.noreply.github.com>
2023-01-05 20:02:38 +0300

    Projeye başlandı, Login oluşturuldu

---
+++ WEB-PROGRAMLAMA-ODEVİ/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_PROGRAMLAMA_ODEVİ.Models;
+
+namespace WEB_PROGRAMLAMA_ODEVİ.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Login işlemleri yapılacak
+            }
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
--- WEB-PROGRAMLAMA-ODEVİ/Program.cs
+++ WEB-PROGRAMLAMA-ODEVİ/Program.cs
@@ -7,7 +7,7 @@ namespace WEB_PROGRAMLAMA_ODEVİ
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             var app = builder.Build();
 
